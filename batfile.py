1599da7474aaecb32e1c6304ce183eabbc4d3822
gagan0183 <g.deepsingh1@gmail.com>
2018-01-30 00:04:05 +0530

    adding timeout

--- batfile.py
+++ batfile.py
@@ -4,6 +4,6 @@ while(cont == "y"):
 	url = str(raw_input("Please enter url : "))
 	totalfile = int(raw_input("Please enter total number of file : "))
 	f = open("p.bat", "a+")
-	f.write('script.py {} "{}" {}\n'.format(coursename, url, totalfile));
+	f.write('script.py {} "{}" {}\ntimeout 513\n'.format(coursename, url, totalfile));
 	f.close()
 	cont = str(raw_input("Do you want to continue (y/n) ? "))
--- p.bat
+++ p.bat
@@ -8,4 +8,5 @@ script.py java-concurrent-patterns-advanced "https://app.pluralsight.com/library
 script.py node-intro-course4 "https://app.pluralsight.com/library/courses/node-intro" 52
 script.py nodejs-express-web-applications-course5 "https://app.pluralsight.com/library/courses/nodejs-express-web-applications" 70
 script.py node-application-patterns-course8 "https://app.pluralsight.com/library/courses/node-application-patterns" 22
-script.py nodejs-advanced-course9 "https://app.pluralsight.com/library/courses/nodejs-advanced" 57
+script.py nodejs-advanced-course9 "https://app.pluralsight.com/library/courses/nodejs-advanced" 
+timeout 513
