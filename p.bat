4a867a5f539c1c2698f6e7864c7ced49fa49eb72
Gagan <g.deepsingh1@gmail.com>
2018-02-25 02:37:16 +0530

    added new courses

--- p.bat
+++ p.bat
@@ -1,12 +1,38 @@
-script.py ionic-end-to-end "https://app.pluralsight.com/library/courses/ionic-end-to-end" 55
-script.py ionic-framework-tools-patterns "https://app.pluralsight.com/library/courses/ionic-framework-tools-patterns" 42
-script.py building-mobile-apps-ionic-framework-angularjs "https://app.pluralsight.com/library/courses/building-mobile-apps-ionic-framework-angularjs" 78
-script.py extjs-desktop-fundamentals "https://app.pluralsight.com/library/courses/extjs-desktop-fundamentals" 86
-script.py android-beginner-platform-overview-course2 "https://app.pluralsight.com/library/courses/android-beginner-platform-overview" 88
-script.py linux-installation-configuration-course1 "https://app.pluralsight.com/library/courses/linux-installation-configuration" 62
-script.py java-concurrent-patterns-advanced "https://app.pluralsight.com/library/courses/java-concurrent-patterns-advanced" 116
-script.py node-intro-course4 "https://app.pluralsight.com/library/courses/node-intro" 52
-script.py nodejs-express-web-applications-course5 "https://app.pluralsight.com/library/courses/nodejs-express-web-applications" 70
-script.py node-application-patterns-course8 "https://app.pluralsight.com/library/courses/node-application-patterns" 22
-script.py nodejs-advanced-course9 "https://app.pluralsight.com/library/courses/nodejs-advanced" 
+script.py puppet-system-administrators-fundamentals-puppetcourse1 "https://app.pluralsight.com/library/courses/puppet-system-administrators-fundamentals" 75
+timeout 513
+script.py puppet-4-language-essentials-puppetcourse2 "https://app.pluralsight.com/library/courses/puppet-4-language-essentials" 42
+timeout 513
+script.py puppet-4-working-modules-classes-puppetcourse3 "https://app.pluralsight.com/library/courses/puppet-4-working-modules-classes" 37
+timeout 513
+script.py puppet-4-files-templates-puppetcourse4 "https://app.pluralsight.com/library/courses/puppet-4-files-templates" 38
+timeout 513
+script.py puppet-4-hiera-puppetcourse5 "https://app.pluralsight.com/library/courses/puppet-4-hiera" 37
+timeout 513
+script.py puppet-4-server-puppet-enterprise-puppetcourse6 "https://app.pluralsight.com/library/courses/puppet-4-server-puppet-enterprise" 35
+timeout 513
+script.py quick-start-javascript-1-jscourse1 "https://app.pluralsight.com/library/courses/quick-start-javascript-1-1870" 13
+timeout 513
+script.py java-fundamentals-language-javacourse1 "https://app.pluralsight.com/library/courses/java-fundamentals-language" 131
+timeout 513
+script.py java-fundamentals-core-platform-javacourse2 "https://app.pluralsight.com/library/courses/java-fundamentals-core-platform" 107
+timeout 513
+script.py java-testing-introduction-javacourse8 "https://app.pluralsight.com/library/courses/java-testing-introduction" 42
+timeout 513
+script.py test-driven-development-java-javacourse9 "https://app.pluralsight.com/library/courses/test-driven-development-java" 50
+timeout 513
+script.py refactoring-fundamentals "https://app.pluralsight.com/library/courses/refactoring-fundamentals" 190
+timeout 513
+script.py principles-oo-design "https://app.pluralsight.com/library/courses/principles-oo-design" 68
+timeout 513
+script.py domain-driven-design-in-practice "https://app.pluralsight.com/library/courses/domain-driven-design-in-practice" 101
+timeout 513
+script.py resharperfundamentals "https://app.pluralsight.com/library/courses/resharperfundamentals" 72
+timeout 513
+script.py time-management-technical-professionals-techinalpath11 "https://app.pluralsight.com/library/courses/time-management-technical-professionals" 28
+timeout 513
+script.py date-time-fundamentals "https://app.pluralsight.com/library/courses/date-time-fundamentals" 68
+timeout 513
+script.py career-reboot-for-developer-mind "https://app.pluralsight.com/library/courses/career-reboot-for-developer-mind" 48
+timeout 513
+script.py learning-technology-information-age "https://app.pluralsight.com/library/courses/learning-technology-information-age" 27
 timeout 513
